c98c007b3a4c0085d84938fa2dacfb3e465f0a4f
joshtin2505 <justincastro2505@gmail.com>
2024-04-04 07:34:09 -0500

    Update CV cron schedule

--- src/scripts/updateCv.js
+++ src/scripts/updateCv.js
@@ -12,27 +12,27 @@ const { Octokit } = require("@octokit/rest");
 
 // Configura tu token de acceso personal
 const octokit = new Octokit({
-  auth: 'token' // hacer por GA
+  auth: process.env.GITHUB_TOKEN   // hacer por GA
 })
 async function updateCV() {
     try {
       // Obtener la lista de repositorios del usuario
       const { data: repos } = await octokit.repos.listForAuthenticatedUser();
-    console.log(repos)
+    // console.log(repos)
       // Encontrar el último commit de todos los repositorios
-      const latestCommit = repos.reduce((latest, repo) => {
-        const pushedAt = new Date(repo.pushed_at);
-        return pushedAt > latest ? pushedAt : latest;
-      }, new Date(0));
+      // const latestCommit = repos.reduce((latest, repo) => {
+      //   const pushedAt = new Date(repo.pushed_at);
+      //   return pushedAt > latest ? pushedAt : latest;
+      // }, new Date(0));
   
       // Actualizar el archivo de CV con la fecha del último commit
       // Ejemplo de escritura en un archivo
-      fs.writeFile('cv.txt', `Último commit: ${latestCommit}`);
-      console.log('CV actualizado correctamente.');
+      const res = await fs.writeFile('git.json', `[${repos.map(repo => repo.stringify())}]`);
+      // fs.writeFile('cv.txt', `Último commit: ${latestCommit}`);
+      // console.log('CV actualizado correctamente.');
     } catch (error) {
-      console.error('Error al actualizar el CV:', error.message);
+      // console.error('Error al actualizar el CV:', error.message);
     }
   }
 updateCV()
-
 // 
