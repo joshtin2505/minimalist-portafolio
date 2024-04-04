5b6b30c7eb81dff307751d98146fd46c049be13a
joshtin2505 <justincastro2505@gmail.com>
2024-04-04 12:59:39 -0500

    Update updateCv.js file

--- src/scripts/updateCv.js
+++ src/scripts/updateCv.js
@@ -1,38 +1,39 @@
-const fs = require('fs').promises
-const path = require('path')
+const fs = require("fs").promises;
+const path = require("path");
 const { Octokit } = require("@octokit/rest");
-
 
 // const cvPath = path.resolve(__dirname, './cv.json')
 // ;(async () => {
-    
+
 //     const data = fs.readFile(cvPath, 'utf8')
 //     const cv = JSON.parse(data)
 // })()
 
 // Configura tu token de acceso personal
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN   // hacer por GA
-})
+  auth: process.env.GITHUB_TOKEN, // hacer por GA
+});
 async function updateCV() {
-    try {
-      // Obtener la lista de repositorios del usuario
-      const { data: repos } = await octokit.repos.listForAuthenticatedUser();
-    // console.log(repos)
-      // Encontrar el último commit de todos los repositorios
-      // const latestCommit = repos.reduce((latest, repo) => {
-      //   const pushedAt = new Date(repo.pushed_at);
-      //   return pushedAt > latest ? pushedAt : latest;
-      // }, new Date(0));
-  
-      // Actualizar el archivo de CV con la fecha del último commit
-      // Ejemplo de escritura en un archivo
-      const res = await fs.writeFile('git.json', `[${repos.map(repo => repo.stringify())}]`);
-      // fs.writeFile('cv.txt', `Último commit: ${latestCommit}`);
-      // console.log('CV actualizado correctamente.');
-    } catch (error) {
-      // console.error('Error al actualizar el CV:', error.message);
-    }
+  try {
+    // Obtener la lista de repositorios del usuario
+    const { data: repos } = await octokit.repos.listForAuthenticatedUser();
+    // Encontrar el último commit de todos los repositorios
+    const latestCommit = repos.reduce((latest, repo) => {
+      const pushedAt = new Date(repo.pushed_at);
+      return pushedAt > latest ? pushedAt : latest;
+    }, new Date(0));
+
+    // Actualizar el archivo de CV con la fecha del último commit
+    // Ejemplo de escritura en un archivo
+    await fs.writeFile(
+      "git.json",
+      `[${repos.map((repo) => JSON.stringify(repo, null, 2))}]`
+    );
+    fs.writeFile("cv.txt", `Último commit: ${latestCommit}`);
+    // console.log('CV actualizado correctamente.');
+  } catch (error) {
+    // console.error('Error al actualizar el CV:', error.message);
   }
-updateCV()
-// 
+}
+updateCV();
+//
