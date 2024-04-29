b9c38e364afe93d2fc6d0772701e748655a40ab6
joshtin2505 <justincastro2505@gmail.com>
2024-04-29 11:23:42 -0500

    Update auto_commit.sh script to create a new folder and print a success message

--- .github/scripts/auto_commit.sh
+++ .github/scripts/auto_commit.sh
@@ -1,18 +1,3 @@
-if [ ! -f ./days/auto-commit.md ]; then
-    echo "- $(date)  " > ./days/auto-commit.md
-    # it create a new file with the current date and time as a list item. the space after the date creates a line break
-    # the > operator creates a new file
-else
-    # Append (add) the current date and time to the existing content of the file as a new list item
-    echo "- $(date)  " >> ./days/auto-commit.md
-    # the >> operator appends the output to the end of the file
-fi
-
-# Add the new file to the staging area
-git add ./days/auto-commit.md
-
-# Make a commit with a message
-git commit -m "Commit automatizado $(date)"
-
-# Push the changes to GitHub
-git push origin main
+combre_carpeta="commit.txt"
+mkdir "$nombre_carpeta"
+echo "Carpeta '$nombre_carpeta' creada exitosamente."
